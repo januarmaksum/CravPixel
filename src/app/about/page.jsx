9b03ac827447b520c62f99af04252a4aba76e1a8
Januar Maksum <januarmaksum@gmail.com>
2024-04-18 09:25:50 +0700

    add ui package

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import Link from "next/link";
+
+export const metadata = {
+  title: "CravPixel - About"
+};
 
 export default function About() {
   return (
-    <div>about</div>
-  )
+    <div className="min-h-screen flex justify-center items-center">
+      <Link href={"/"} className="btn btn-primary">
+        Home
+      </Link>
+    </div>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-      home
+    <main className="min-h-screen flex justify-center items-center">
+      <Link href={'/about'} className="btn btn-primary">About</Link>
     </main>
   );
 }
